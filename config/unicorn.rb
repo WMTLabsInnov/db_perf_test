51c4089d365cfb4c7cc33bf2cd978df486b78f41
victor-huang <vhuang@walmartlabs.com>
2013-08-14 10:02:42 -0700

    make start unicron easier

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = ENV['DYNAMIC_DEALS_ROOT'] || '/Users/vhuang2/work/flash_deal' #Rails project root
+root = ENV['DB_PERF_TEST_ROOT'] || '/root/db_perf_test' #Rails project root
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
 listen 8080
-worker_processes 8
+worker_processes 3
 timeout 30
